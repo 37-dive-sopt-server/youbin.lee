9f2788d75d4570644564d5c3f87ee9b0ca5b64f2
LEE YOU BIN <youbeene3409@naver.com>
2025-10-14 21:01:39 +0900

    feat : domain -> Member 변경

--- src/main/java/org/sopt/domain/Member.java
+++ src/main/java/org/sopt/domain/Member.java
@@ -1,13 +1,21 @@
 package org.sopt.domain;
 
+import java.time.LocalDate;
+
 public class Member {
 
     private Long id;
     private String name;
+    private LocalDate birthDate;
+    private String email;
+    private Gender gender;
 
-    public Member(Long id, String name) {
+    public Member(Long id, String name, LocalDate birthDate, String email, Gender gender) {
         this.id = id;
         this.name = name;
+        this.birthDate = birthDate;
+        this.email = email;
+        this.gender = gender;
     }
 
     public Long getId() {
@@ -16,5 +24,17 @@ public class Member {
 
     public String getName() {
         return name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Gender getGender() {
+        return gender;
     }
 }
