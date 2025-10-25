7aca80946f8bebbdc2f6f80349fa82d11328ace2
LEE YOU BIN <youbeene3409@naver.com>
2025-10-26 03:15:09 +0900

    feat : findMember 리팩토링

--- src/main/java/org/sopt/ui/MemberController.java
+++ src/main/java/org/sopt/ui/MemberController.java
@@ -6,10 +6,7 @@ import org.sopt.common.validator.EmailValidator;
 import org.sopt.common.validator.NameValidator;
 import org.sopt.domain.Member;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,8 +26,10 @@ public class MemberController {
         return memberService.join(request);
     }
 
-    @GetMapping("/users")
-    public Member findMember(Long id) {
+    @GetMapping("/users/{id}")
+    public Member findMember(
+            @PathVariable Long id
+    ) {
         return memberService.findByIdOrThrow(id);
     }
 
@@ -38,7 +37,6 @@ public class MemberController {
     public List<Member> getAllMembers() {
         return memberService.findAllMembers();
     }
-
 
     public boolean deleteMemberById(Long id) {
         return memberService.deleteId(id);
