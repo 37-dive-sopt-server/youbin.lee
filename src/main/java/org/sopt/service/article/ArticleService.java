14789862f4e1e5f95674d35093324e42ea2d1023
LEE YOU BIN <youbeene3409@naver.com>
2025-11-07 01:07:28 +0900

    feat : 아티클 서비스 인터페이스 구현

---
+++ src/main/java/org/sopt/service/article/ArticleService.java
@@ -0,0 +1,12 @@
+package org.sopt.service.article;
+
+import org.sopt.service.article.dto.request.ArticleCreateRequest;
+import org.sopt.service.article.dto.response.ArticleGetResponse;
+
+import java.util.List;
+
+public interface ArticleService {
+    void create(ArticleCreateRequest request);
+    ArticleGetResponse getArticle(Long id);
+    List<ArticleGetResponse> getArticleList();
+}
