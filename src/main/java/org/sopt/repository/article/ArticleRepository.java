896b51cd9638df90b0ec1f19722e95777bed88ac
LEE YOU BIN <youbeene3409@naver.com>
2025-11-07 01:07:19 +0900

    feat : 아티클 레포지토리 구현

---
+++ src/main/java/org/sopt/repository/article/ArticleRepository.java
@@ -0,0 +1,7 @@
+package org.sopt.repository.article;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.sopt.domain.article.Article;
+
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+}
