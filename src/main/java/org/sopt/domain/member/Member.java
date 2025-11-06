cbf1e1040f810bbcf773b24ae8eef1f37e1ae67e
LEE YOU BIN <youbeene3409@naver.com>
2025-11-07 01:44:03 +0900

    feat : 기존 member 엔티티 수정

--- src/main/java/org/sopt/domain/member/Member.java
+++ src/main/java/org/sopt/domain/member/Member.java
@@ -1,23 +1,39 @@
-package org.sopt.domain;
+package org.sopt.domain.member;
+
+import jakarta.persistence.*;
+import org.sopt.domain.article.Article;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+@Entity(name = "member")
 public class Member {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+
     private LocalDate birthDate;
     private String email;
+    @Enumerated(EnumType.STRING)
     private Gender gender;
 
+    @OneToMany(mappedBy = "member")
+    private List<Article> articles = new ArrayList<>();
+
+    public Member() {
+    }
+
     public Member(
-            Long id,
             String name,
             LocalDate birthDate,
             String email,
             Gender gender
     ) {
-        this.id = id;
         this.name = name;
         this.birthDate = birthDate;
         this.email = email;
