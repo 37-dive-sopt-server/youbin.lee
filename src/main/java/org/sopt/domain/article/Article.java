da6f3057353a165ff86ec97dfacca20abb20a011
LEE YOU BIN <youbeene3409@naver.com>
2025-11-07 12:16:45 +0900

    feat : CreatedDate 사용

--- src/main/java/org/sopt/domain/article/Article.java
+++ src/main/java/org/sopt/domain/article/Article.java
@@ -3,12 +3,15 @@ package org.sopt.domain.article;
 import jakarta.persistence.*;
 import lombok.*;
 import org.sopt.domain.member.Member;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
 
 @Entity(name = "article")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
+@EntityListeners(AuditingEntityListener.class)
 @Builder
 @Getter
 public class Article {
@@ -25,6 +28,7 @@ public class Article {
     @Column(nullable = false)
     private Tag tag;
 
+    @CreatedDate
     @Column(nullable = false)
     private LocalDate createAt;
 
