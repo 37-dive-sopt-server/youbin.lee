4f3894f50a6737acad0a5c01dba0f44057069e38
LEE YOU BIN <youbeene3409@naver.com>
2025-12-22 14:07:40 +0900

    feat : comment 엔티티 작성

---
+++ src/main/java/org/sopt/domain/commnet/Comment.java
@@ -0,0 +1,50 @@
+package org.sopt.domain.commnet;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.sopt.domain.article.Article;
+import org.sopt.domain.member.Member;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity(name = "comment")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@EntityListeners(AuditingEntityListener.class)
+@Builder
+@Getter
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 300)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "article_id")
+    private Article article;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "member_id")
+    private Member writer;
+
+    @Builder
+    private Comment(String content, Article article, Member writer) {
+        this.content = content;
+        this.article = article;
+        this.writer = writer;
+    }
+
+    public static Comment of(String content, Article article, Member writer) {
+        return Comment.builder()
+                .content(content)
+                .article(article)
+                .writer(writer)
+                .build();
+    }
+
+    public void updateContent(String newContent) {
+        this.content = newContent;
+    }
+}
