e136f8c902c11058f889f30ef1aec564c70792a9
LEE YOU BIN <youbeene3409@naver.com>
2025-12-22 15:30:35 +0900

    feat : 댓글 전체 조회 repository 구현

--- src/main/java/org/sopt/repository/comment/CommentRepository.java
+++ src/main/java/org/sopt/repository/comment/CommentRepository.java
@@ -1,7 +1,10 @@
 package org.sopt.repository.comment;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.sopt.domain.commnet.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findAllByArticleId(Long articleId);
 }
